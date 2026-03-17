b496ee4021bf2bbdd23ca4b1203486a2aab3c38e
Gustavo Picoli <gustavo.picoli@hotmail.com>
2026-03-17 19:58:49 -0300

    Criação de classe

---
+++ GitHub Desktop Exemplo/src/github/desktop/exemplo/Game12.java
@@ -0,0 +1,17 @@
+package github.desktop.exemplo;
+
+    
+    //starting main function
+public class Game12 {
+    public static void main(String[] args) {
+        Game12 gaming=new Game12();
+        boolean b=false;
+        while (!b) {
+            gaming.TakeUserinput();
+            b=gaming.NoofCorrectGuesses();
+        }
+
+    }
+}
+    
+
